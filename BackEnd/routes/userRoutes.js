d8ea1916a20fda27299c57910cf57b36a3d87d33
Ashish Bajaj <bajajashu97@gmail.com>
2025-09-13 17:43:35 +0530

    remove backend and frontend

--- BackEnd/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { loginUser } from '../controllers/userController.js';
-
-const router = express.Router();
-
-// Only login route
-router.post('/login', loginUser);
-
-export default router;
